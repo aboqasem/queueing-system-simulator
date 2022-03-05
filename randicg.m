005908c43725e680539b77ead656fc565f046579
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2022-03-05 14:55:32 +0800

    implement an ICG random number generator

---
+++ randicg.m
@@ -0,0 +1,35 @@
+% Inversive Congruential Generator (ICG)
+% ICGs have the form: Xₙ = (a(Xₙ₋₁)⁻¹ + c) mod m
+% Where X₀ is the seed (starting value), 0 ≤ X₀ < m
+% m is the modulus, 0 < m
+% a is the multiplier, 0 < a < m
+% c is the increment, 0 ≤ c < m
+% Xₙ is the positive remainder when we divide (a(Xₙ₋₁)⁻¹ + c) by m, thus; 0 ≤ Xₙ < m.
+
+function x = randicg()
+	% ICG_SEED is the starting value (X₀), ICG_PREV_X is Xₙ₋₁
+	global ICG_SEED ICG_PREV_X;
+
+	% no seed means n is currently 0
+	if (~isset('ICG_SEED'))
+		% random starting value
+		ICG_SEED = rand();
+		% X₀ is the seed
+		x = ICG_SEED;
+
+		ICG_PREV_X = x;
+		return;
+	end
+
+	% Xₙ₋₁
+	prevX = ICG_PREV_X;
+	% arbitrarily assumed multiplier and increment
+	a = 0.5; c = 0.7;
+	% modulus is 1 to make sure 0 ≤ Xₙ < 1
+	m = 1;
+
+	% Xₙ = (a(Xₙ₋₁)⁻¹ + c) mod m
+	x = mod(a * (prevX ^ -1) + c, m);
+
+	ICG_PREV_X = x;
+end
