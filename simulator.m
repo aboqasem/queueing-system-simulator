ca2208435e9b27c9bab550b88b11e6dd56f22e4b
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2022-03-05 15:52:31 +0800

    get simulator input arguments by user

--- simulator.m
+++ simulator.m
@@ -1,1 +1,19 @@
-printf('\t\t\t\tWelcome to the simulator!\n');
+% Random function to be picked by the user
+global randfn;
+
+printf('Welcome to the simulator!\n\n');
+
+nPatients = str2num(getinput('Number of patients in this simulation: '));
+
+printf('---------------------------------------------------------------\n');
+printf('               Random Number Generation Type                   \n');
+printf('---------------------------------------------------------------\n');
+printf('1. Linear Congruential Generator (LCG)\n');
+printf('2. Inversive Congruential Generator (ICG)\n');
+printf('---------------------------------------------------------------\n');
+randType = getinput('Generator type number: ', @(str) any(strcmp(str, {'1', '2'})), 'Invalid generator type number.');
+if (strcmp(randType, '1'))
+    randfn = @randlcg;
+else
+    randfn = @randicg;
+end
