ae77daa3039c36cd738f4cc88ee3acf800be8643
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2022-03-14 18:46:40 +0800

    encapsulate random function initialization to a function

---
+++ initrandfn.m
@@ -0,0 +1,18 @@
+function initrandfn()
+  global randfn;
+
+  printf('---------------------------------------------------------------\n');
+  printf('               Random Number Generation Type                   \n');
+  printf('---------------------------------------------------------------\n');
+  printf('1. Linear Congruential Generator (LCG)\n');
+  printf('2. Inversive Congruential Generator (ICG)\n');
+  printf('---------------------------------------------------------------\n');
+  randType = getinput('Generator type number: ', @(str) any(strcmp(str, {'1', '2'})), 'Invalid generator type number.');
+  printf('\n\n');
+
+  if (strcmp(randType, '1'))
+      randfn = @randlcg;
+  else
+      randfn = @randicg;
+  end
+end
--- simulator.m
+++ simulator.m
@@ -4,20 +4,8 @@ global randfn;
 printf('Welcome to the simulator!\n\n');
 
 nPatients = str2num(getinput('Number of patients in this simulation: ', @(str) str2num(str) > 0));
-
-printf('---------------------------------------------------------------\n');
-printf('               Random Number Generation Type                   \n');
-printf('---------------------------------------------------------------\n');
-printf('1. Linear Congruential Generator (LCG)\n');
-printf('2. Inversive Congruential Generator (ICG)\n');
-printf('---------------------------------------------------------------\n');
-randType = getinput('Generator type number: ', @(str) any(strcmp(str, {'1', '2'})), 'Invalid generator type number.');
-if (strcmp(randType, '1'))
-    randfn = @randlcg;
-else
-    randfn = @randicg;
-end
-
 printf('\n\n');
+
+initrandfn();
 
 [interarrivalTimes, interarrivalTimesRanges] = initinterarrivaltimesdata(5);
